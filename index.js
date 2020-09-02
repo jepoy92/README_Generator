0f3cf3bcb787d3b82c8e6a0aed70cc451aad2520
Jeffrey Choi <jeffrey1choi@gmail.com>
2020-09-01 17:09:36 -0700

    updated readme format

--- index.js
+++ index.js
@@ -57,11 +57,6 @@ function promptUser() {
   ]);
 }
 
-* Table of Contents
-* Tests
-* Questions
-
-
 function generateHTML(answers) {
   return `
   # ${answers.title}
@@ -71,39 +66,35 @@ function generateHTML(answers) {
 ${answers.description}
 
 
-## Table of Contents (Optional)
+## Table of Contents
 
 If your README is very long, add a table of contents to make it easy for users to find what they need.
 
 * [Installation](#installation)
 * [Usage](#usage)
-* [Credits](#credits)
 * [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-
+${answers.installation}
 
 ## Usage 
 
-Provide instructions and examples for use. Include screenshots as needed. 
+${answers.useage} 
 
 
-## Credits
+## Contributing
 
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
+${answers.contributing}
 
 
 ## License
 
-The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
+${answers.license}
 
 
 ---
